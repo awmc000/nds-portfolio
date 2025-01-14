393fd542ec8a2759c7043d0bd5d49aaab8fe51aa
Alex McColm <mccolmalex@gmail.com>
2025-01-14 12:37:40 -0800

    A few more Maze tests, and remove test binary after each run

--- test/test_maze.cpp
+++ test/test_maze.cpp
@@ -4,9 +4,23 @@
 START_TEST (test_maze_build) {
     Maze maze;
     maze.build();
+}
+END_TEST
+
+START_TEST (test_maze_get_cell) {
+    Maze maze;
+    maze.build();
     ck_assert_int_eq(maze.getCell(0, 0), 1);
     ck_assert_int_eq(maze.getCell(5, 0), 1);
     ck_assert_int_eq(maze.getCell(1, 1), 0);
+}
+END_TEST
+
+START_TEST (test_maze_get_cell_out_of_bounds) {
+    Maze maze;
+    maze.build();
+    ck_assert_int_eq(maze.getCell(0, MAZE_WIDTH), -1);
+    ck_assert_int_eq(maze.getCell(-50, 0), -1);
 }
 END_TEST
 
@@ -19,6 +33,9 @@ Suite * maze_suite(void) {
     tc_maze = tcase_create("Maze");
 
     tcase_add_test(tc_maze, test_maze_build);
+    tcase_add_test(tc_maze, test_maze_get_cell);
+    tcase_add_test(tc_maze, test_maze_get_cell_out_of_bounds);
+    
     suite_add_tcase(s, tc_maze);
 
     return s;
